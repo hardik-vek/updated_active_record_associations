6e03d8c609f7f8596f3f430d705f15796cd0aab3
hardik <hardikvekariya09@gmail.com>
2022-03-01 16:52:35 +0530

    bug fixed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,8 @@
 class EventsController < ApplicationController
   # before_action :logged_in?
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  before_action :find_event, only: [:show, :edit, :update, :destroy]
 
   def index
     @events = Event.all
